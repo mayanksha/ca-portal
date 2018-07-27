f0c9122ae4bde7f88b73a07b981bfa6d98ad3926
Mayank Sharma <msharma@iitk.ac.in>
2018-07-27 19:12:27 +0530

    Added mysql to sheets feature

--- server/sheets_server.ts
+++ server/sheets_server.ts
@@ -1,102 +1,104 @@
+import * as mysql from 'mysql';
+
+/*import { logger } from './config/logger';*/
 import { google, sheets_v4 } from 'googleapis';
 import { Database } from './config/database';
-import * as mysql from 'mysql';
 import { Startup } from './interfaces/startup'; 
-import { localConfig } from './config/local_config';
-/*import { sheets }*/
+import { localConfig, sheetsConfig } from './config/local_config';
+
 class DatabaseToSheets {
 
+	private scopes = ['https://www.googleapis.com/auth/spreadsheets'];
 	private sheets: any;
 	private db: Database;
-	private allStartups: Startup[] = [];
+
+	public JWTClient;
 	constructor() {
+		this.JWTClient = new google.auth.JWT(sheetsConfig.client_email,
+			undefined, 
+			sheetsConfig.private_key,
+			this.scopes,
+			undefined);	
 		this.db = Database.getInstance();	
-		this.sheets = google.sheets({
-			version: 'v4',
-			auth: localConfig.apiToken
-		})
+		this.sheets = google.sheets('v4');
 	}
-	public fetchAllData(): Promise<Startup[]> {
-		const query = `SELECT * FROM registrations.upstart`;
+
+	private fetchAllData(): Promise<any> {
+		const query = `SELECT up.id,up.facebookID, up.startupName, up.email, up.numPersons,
+		up.phone, up.location, up.eventName, ipm.PersonName, links.link
+		FROM registrations.upstart as up inner
+		join id_person_mapping as ipm on up.facebookID=ipm.facebookID
+		left join links on up.facebookID=links.facebookID order by up.id`;
+
 		let personsQuery = `SELECT * FROM registrations.id_person_mapping`;
 		let startupArray: Startup[] = [];
 		let allStartups = [];	
 		let allPersons = [];
 
-		let startupsPromise = this.db.query(query)
-			.then(rows => Array.from(rows as any))
-			.then((arr: any) => allStartups = arr)
-			.catch(err => console.error(err));
-		let personsPromise = this.db.query(personsQuery)
-			.then(rows => Array.from(rows as any))
-			.then((arr: any) => allPersons = arr)
-			.catch(err => console.error(err));
-			
-		return Promise.all([startupsPromise, personsPromise])
-			.then(() => {
-				allStartups.forEach((e: any) => {
-					let fbID = e.facebookID;
-					let members = (allPersons.filter((elem: any) => elem.facebookID === fbID))
-						.map((e: any) => e.PersonName);
-					console.log(members);
-					startupArray.push({
-						startupName: e.startupName,
-						numPersons: e.numPersons,
-						allPersons: members,
-						contactNo: e.phone,
-						email: e.email,
-						location: e.location,
-						eventName: e.eventName,
-					})	
-				})
-				return (this.allStartups = startupArray);
-			})
-			.catch(err => {
-				console.error
-				return Promise.reject(err);
-			});
+		return this.db.query(query)
+		.then(rows => {
+			let values: Startup[] = JSON.parse(JSON.stringify(rows));
+			values = values.map((elem) => (Object as any).values(elem));
+			/*console.log(values);*/
+			return values;
+		})
+		.catch(err => {
+			/*logger.log('error', err);*/
+			console.error(err);
+		})
 	}
-	
-	public writeToSheet() {
-		this.fetchAllData()
-			.then(e => {
-				let values = [
-						['startupName',
-						'numPersons',
-						'allPersons',
-						'contactNo',
-						'email',
-						'location',
-						'eventName',]
-				]
-				let body = {
-					values: values
-				}
+
+	private authorize(): Promise<any> {
+		return new Promise((resolve, reject) => {
+			this.JWTClient.authorize((err, tokens) => {
+				if (err)
+					return reject(err);
+				else return resolve(tokens);
+			})
+		})
+	}
+
+	public writeToSheet(): Promise<any> {
+		return this.fetchAllData()
+		.then((data) => {
+			let body = {
+				values: data 
+			}
+			return body;
+		})
+		.then((sheetBody: any) => {
+			return this.authorize()
+				.then((tokens) => [tokens, sheetBody])
+		})
+		.then((TokenSheetBodyArray: any[]) => {
+			return new Promise((resolve, reject) => {
 				this.sheets.spreadsheets.values.update({
 					spreadsheetId: localConfig.sheet,
 					valueInputOption: "RAW",
-					resource: body,
-					range: 'A:G',
-					key: localConfig.apiToken
+					resource: TokenSheetBodyArray[1],
+					range: 'A2:J',
+					auth: this.JWTClient
 				}, (err, result) => {
-					if(err)
-						console.error(err);
-					else 
-						console.log(result);
+					if(err){
+						return reject(err);
+					}
+					else {
+						return resolve(result);
+					}
 				})
-				/*values = values.concat(e as any);
-				 *console.log(values);*/
-				/*this.sheets.spreadsheets.values.updatee({
-				 *  spreadsheetId: localConfig.sheet,
-				 *  valueInputOption: "RAW",
-				 *  resource: body
-				 *})*/
 			})
+				.then((result: any) => result.data)
+				.catch((err) => {
+					console.error(err);
+					return Promise.reject(err);
+				})
+		})
 	}
-	
 }
-
 const DTS = new DatabaseToSheets();
-DTS.writeToSheet();
-
-
+setTimeout(() => {
+	DTS.writeToSheet();
+}, 100);
+/*setInterval(() => {
+ *  DTS.writeToSheet();
+ *}, 5000);*/
